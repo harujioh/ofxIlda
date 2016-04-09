f36b90620bdf8c83fb59f65ae83c861fe4ef091a
hirata <hirata@team-lab.com>
2016-04-09 19:04:23 +0900

    format

--- src/ofxIldaPoly.h
+++ src/ofxIldaPoly.h
@@ -9,24 +9,28 @@
 #pragma once
 
 namespace ofxIlda {
-    
-	class Poly: public ofPolyline{
-	public:
-		ofFloatColor color;
-		Poly() : color(ofFloatColor(1, 1, 1, 1)) {}
 
-        Poly(ofFloatColor color) : color(color) {}
-        
-        Poly(const Poly& poly) : ofPolyline(poly), color(poly.color) {}
-        
-        Poly(const ofPolyline& polyline) : ofPolyline(polyline), color(ofFloatColor(1, 1, 1, 1)) {}
-        
-        Poly(const ofPolyline& polyline, ofFloatColor color) : ofPolyline(polyline), color(color) {}
+class Poly : public ofPolyline {
+   public:
+    ofFloatColor color;
+    Poly() : color(ofFloatColor(1, 1, 1, 1)) {}
 
-		Poly(const vector<ofPoint>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)) {}
+    Poly(ofFloatColor color) : color(color) {}
 
-		Poly(const vector<ofPoint>& verts, ofFloatColor color) : ofPolyline(verts), color(color) {}
-        
-        void setFromPolyline(const ofPolyline& polyline) { ofFloatColor tmpColor = color; *this = polyline; color = tmpColor; }
-	};
+    Poly(const Poly& poly) : ofPolyline(poly), color(poly.color) {}
+
+    Poly(const ofPolyline& polyline) : ofPolyline(polyline), color(ofFloatColor(1, 1, 1, 1)) {}
+
+    Poly(const ofPolyline& polyline, ofFloatColor color) : ofPolyline(polyline), color(color) {}
+
+    Poly(const vector<ofPoint>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)) {}
+
+    Poly(const vector<ofPoint>& verts, ofFloatColor color) : ofPolyline(verts), color(color) {}
+
+    void setFromPolyline(const ofPolyline& polyline) {
+        ofFloatColor tmpColor = color;
+        *this = polyline;
+        color = tmpColor;
+    }
+};
 }
